b692df652f71dc4651710908722750c326681a5b
tq <tilekbergen.tiqu@gmail.com>
2024-05-28 12:43:05 +0500

    added editing and deleting functionality

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ const inputContext = ref("");
 const category = ref("personal");
 const todoArr = ref([]);
 const filteredArr = ref([]);
+const createdAt = ref(null);
+const edit = ref(false);
 
 onMounted(() => {
   name.value = localStorage.getItem("name") || "";
@@ -17,7 +19,9 @@ onMounted(() => {
 
 const addTodo = () => {
   if (inputContext.value.trim() === "" || category.value === null) {
-  } else {
+    return;
+  }
+  if (edit.value === false) {
     todoArr.value.push({
       todo: inputContext.value,
       category: category.value,
@@ -25,7 +29,33 @@ const addTodo = () => {
       done: false,
     });
     inputContext.value = "";
+  } else {
+    todoArr.value.forEach((e) => {
+      if (e.createdAt === createdAt.value) {
+        (e.todo = inputContext.value),
+          (e.category = category.value),
+          (e.createdAt = new Date().getTime()),
+          (e.done = false);
+        return e;
+      }
+      return e;
+    });
+    inputContext.value = "";
+    edit.value = false;
   }
+};
+
+const handleDelete = (e) => {
+  todoArr.value = todoArr.value.filter((todo) => {
+    return todo.createdAt !== e.createdAt;
+  });
+};
+
+const handleEdit = (e) => {
+  edit.value = true;
+  inputContext.value = e.todo;
+  category.value = e.category;
+  createdAt.value = e.createdAt;
 };
 
 watch(name, (value) => {
@@ -36,7 +66,6 @@ watch(category, (cat) => {
   filteredArr.value = todoArr.value.filter((e) => {
     return e.category === cat;
   });
-  console.log(filteredArr);
 });
 
 watch(
@@ -67,7 +96,7 @@ watch(
       <p>Create a to do!</p>
       <input
         type="text"
-        name=""
+        name="todo"
         v-model="inputContext"
         class="input"
         placeholder="Start writing..."
@@ -82,19 +111,34 @@ watch(
           />
           <p>Personal</p>
         </div>
+        <div></div>
         <div class="category-box">
           <input type="radio" name="category" v-model="category" value="work" />
           <p>Work</p>
         </div>
       </div>
-      <input type="submit" name="Submit" value="Add" class="button" />
+
+      <input
+        v-if="!edit"
+        type="submit"
+        name="Submit"
+        value="Add"
+        class="button"
+      />
+      <input
+        v-if="edit"
+        type="submit"
+        name="Submit"
+        value="Edit"
+        class="button"
+      />
     </form>
 
     <ul>
       <li v-for="(item, index) in filteredArr" :key="index">
         <p>{{ item.todo }}</p>
-        <button class="ml-4">Edit</button>
-        <button class="ml-4 success">Done</button>
+        <button @click="handleEdit(item)" class="ml-4">Edit</button>
+        <button @click="handleDelete(item)" class="ml-4 success">Done</button>
       </li>
     </ul>
   </div>
